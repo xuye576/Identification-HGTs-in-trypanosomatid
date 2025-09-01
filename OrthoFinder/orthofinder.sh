b59f869147f6887f2f1ac61af8547eca223a4645
xuye576 <yuanxuye@163.com>
2025-09-01 15:45:55 +0800

    Add files via upload

---
+++ OrthoFinder/orthofinder.sh
@@ -0,0 +1,5 @@
+cd ./OrthoFinder
+python3.6 ./OrthoFinder_source/orthofinder.py -f protein/
+python3.6 ./OrthoFinder_source/orthofinder.py -f HGTfasta/
+python3.6 ./OrthoFinder_source/orthofinder.py -f nonHGTfasta/
+
